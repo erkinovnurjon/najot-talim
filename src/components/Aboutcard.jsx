0917e01de4e839d6e84f4d8a65b35e7c604ecbb5
anvarbeknorimov <anvarbeknorimov9846@gmail.com>
2023-08-19 18:14:58 +0500

    about pages

--- src/components/Aboutcard.jsx
+++ src/components/Aboutcard.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const Aboutcard = () => {
   return (
-    <div className="container  about__card">
-      <ul className="about__card-list">
+    <div>
+      <ul className="about__card-list container">
         <li className="about__card-list-item">
           <Link className="about__card-link">
             <div className="about__card-link-top">
@@ -22,6 +22,166 @@ const Aboutcard = () => {
               </div>
               <div className="about__card-link-bottom-right">
                 <img
+                  className="about__card-link-bottom-right-images"
+                  src="https://cdn.najottalim.uz/najottalim/courses/illustrations/95b2a0a8-84b1-4257-8006-875a0351f561.svg"
+                  alt="icon"
+                />
+              </div>
+            </div>
+          </Link>
+        </li>
+        <li className="about__card-list-item">
+          <Link className="about__card-link">
+            <div className="about__card-link-top">
+              <span className="about__card-link-top-title">Dizayn</span>
+            </div>
+            <div className="about__card-link-bottom">
+              <div className="about__card-link-bottom-left">
+                <h3 className="about__card-link-bottom-left-title">
+                  Social Media Design
+                </h3>
+                <p className="about__card-link-bottom-left-subtitle">
+                  Ijtimoiy tarmoqlar uchun chiroyli va samarali postlar
+                  tayyorlashni o'rganing.
+                </p>
+              </div>
+              <div className="about__card-link-bottom-right">
+                <img
+                  className="about__card-link-bottom-right-images"
+                  src="https://cdn.najottalim.uz/najottalim/courses/illustrations/42e5e0f8-f5b9-4944-bbd2-b838fca0023c.svg"
+                  alt="icon"
+                />
+              </div>
+            </div>
+          </Link>
+        </li>
+        <li className="about__card-list-item">
+          <Link className="about__card-link">
+            <div className="about__card-link-top">
+              <span className="about__card-link-top-title">Dasturlash</span>
+            </div>
+            <div className="about__card-link-bottom">
+              <div className="about__card-link-bottom-left">
+                <h3 className="about__card-link-bottom-left-title">
+                  Dasturlash foundation
+                </h3>
+                <p className="about__card-link-bottom-left-subtitle">
+                  Foundation dasturlash kursida C va Python dasturlash tillari
+                  va ular orqali dasturlashning mantiq-mohiyati o’rgatiladi.
+                  Kurs davomida dasturlashning fundamental tushunchalari,
+                  dasturchining ishini yengillashtirib beradigan texnologiyalar,
+                  interfeys va ma’lumotlar bazasi bilan ishlash jarayonlari
+                  haqida ma’lumotlar beriladi.
+                </p>
+              </div>
+              <div className="about__card-link-bottom-right">
+                <img
+                  className="about__card-link-bottom-right-images"
+                  src="https://cdn.najottalim.uz/najottalim/courses/illustrations/229ae2fc-3ebf-4659-be28-08fda1a88d48.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        </li>
+        <li className="about__card-list-item">
+          <Link className="about__card-link">
+            <div className="about__card-link-top">
+              <span className="about__card-link-top-title">Dizayn</span>
+            </div>
+            <div className="about__card-link-bottom">
+              <div className="about__card-link-bottom-left">
+                <h3 className="about__card-link-bottom-left-title">
+                  SMM onlayn
+                </h3>
+                <p className="about__card-link-bottom-left-subtitle">
+                  Adobe Illustrator - vektorli grafikalar (chizmalar) bilan
+                  ishlash uchun mo'ljallangan dastur hisoblanadi.
+                </p>
+              </div>
+              <div className="about__card-link-bottom-right">
+                <img
+                  className="about__card-link-bottom-right-images"
+                  src="	https://cdn.najottalim.uz/najottalim/courses/illustrations/e888ff34-190d-41f2-b317-0fd1b0a34506.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        </li>
+        <li className="about__card-list-item">
+          <Link className="about__card-link">
+            <div className="about__card-link-top">
+              <span className="about__card-link-top-title">Dasturlash</span>
+            </div>
+            <div className="about__card-link-bottom">
+              <div className="about__card-link-bottom-left">
+                <h3 className="about__card-link-bottom-left-title">
+                  .NET mutaxassislik kursi
+                </h3>
+                <p className="about__card-link-bottom-left-subtitle">
+                  .NET mutaxassislik kursi yordamida universal C# dasturlash
+                  tilida ishlovchi va oʻz faoliyatida .NET platformasi va
+                  texnologiyalaridan foydalanuvchi dasturchiga aylaning. C#
+                  dasturlash tilining imkoniyatlari va qoʻllash doirasi
+                  nihoyatda keng, uni mukammal bilgan dasturchi esa
+                  veb-saytlardan tortib mobil va desktop ilovalargacha,
+                  oʻyinlardan tortib kross-platformali dasturlargacha qurish
+                  imkoniyatiga ega boʻladi.
+                </p>
+              </div>
+              <div className="about__card-link-bottom-right">
+                <img
+                  className="about__card-link-bottom-right-images"
+                  src="https://cdn.najottalim.uz/najottalim/courses/illustrations/0450bd22-13c5-47c9-9a4f-68093737e73e.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        </li>
+        <li className="about__card-list-item">
+          <Link className="about__card-link">
+            <div className="about__card-link-top">
+              <span className="about__card-link-top-title">Dasturlash</span>
+            </div>
+            <div className="about__card-link-bottom">
+              <div className="about__card-link-bottom-left">
+                <h3 className="about__card-link-bottom-left-title">
+                  Node JS Telegram bot
+                </h3>
+                <p className="about__card-link-bottom-left-subtitle">
+                  Telegram bot yasash va dasturlashni 0 dan o'rganing!
+                </p>
+              </div>
+              <div className="about__card-link-bottom-right">
+                <img
+                  className="about__card-link-bottom-right-images"
+                  src="https://cdn.najottalim.uz/najottalim/courses/illustrations/6c079986-e4e1-4577-b6c4-ad52556c6c9a.svg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        </li>
+        <li className="about__card-list-item">
+          <Link className="about__card-link">
+            <div className="about__card-link-top">
+              <span className="about__card-link-top-title">MArketing</span>
+            </div>
+            <div className="about__card-link-bottom">
+              <div className="about__card-link-bottom-left">
+                <h3 className="about__card-link-bottom-left-title">
+                  SMM onlayn
+                </h3>
+                <p className="about__card-link-bottom-left-subtitle">
+                  Ijtimoiy tarmoqlar orqali turli biznes va loyihalarni
+                  rivojlantirishni o'rganasiz.
+                </p>
+              </div>
+              <div className="about__card-link-bottom-right">
+                <img
+                  className="about__card-link-bottom-right-images"
                   src="https://cdn.najottalim.uz/najottalim/courses/illustrations/95b2a0a8-84b1-4257-8006-875a0351f561.svg"
                   alt=""
                 />
@@ -29,8 +189,6 @@ const Aboutcard = () => {
             </div>
           </Link>
         </li>
-      </ul>
-      <ul className="about__card-list">
         <li className="about__card-list-item">
           <Link className="about__card-link">
             <div className="about__card-link-top">
@@ -48,136 +206,7 @@ const Aboutcard = () => {
               </div>
               <div className="about__card-link-bottom-right">
                 <img
-                  src="https://cdn.najottalim.uz/najottalim/courses/illustrations/95b2a0a8-84b1-4257-8006-875a0351f561.svg"
-                  alt=""
-                />
-              </div>
-            </div>
-          </Link>
-        </li>
-      </ul>
-      <ul className="about__card-list">
-        <li className="about__card-list-item">
-          <Link className="about__card-link">
-            <div className="about__card-link-top">
-              <span className="about__card-link-top-title">MArketing</span>
-            </div>
-            <div className="about__card-link-bottom">
-              <div className="about__card-link-bottom-left">
-                <h3 className="about__card-link-bottom-left-title">
-                  SMM onlayn
-                </h3>
-                <p className="about__card-link-bottom-left-subtitle">
-                  Ijtimoiy tarmoqlar orqali turli biznes va loyihalarni
-                  rivojlantirishni o'rganasiz.
-                </p>
-              </div>
-              <div className="about__card-link-bottom-right">
-                <img
-                  src="https://cdn.najottalim.uz/najottalim/courses/illustrations/95b2a0a8-84b1-4257-8006-875a0351f561.svg"
-                  alt=""
-                />
-              </div>
-            </div>
-          </Link>
-        </li>
-      </ul>
-      <ul className="about__card-list">
-        <li className="about__card-list-item">
-          <Link className="about__card-link">
-            <div className="about__card-link-top">
-              <span className="about__card-link-top-title">MArketing</span>
-            </div>
-            <div className="about__card-link-bottom">
-              <div className="about__card-link-bottom-left">
-                <h3 className="about__card-link-bottom-left-title">
-                  SMM onlayn
-                </h3>
-                <p className="about__card-link-bottom-left-subtitle">
-                  Ijtimoiy tarmoqlar orqali turli biznes va loyihalarni
-                  rivojlantirishni o'rganasiz.
-                </p>
-              </div>
-              <div className="about__card-link-bottom-right">
-                <img
-                  src="https://cdn.najottalim.uz/najottalim/courses/illustrations/95b2a0a8-84b1-4257-8006-875a0351f561.svg"
-                  alt=""
-                />
-              </div>
-            </div>
-          </Link>
-        </li>
-      </ul>
-      <ul className="about__card-list">
-        <li className="about__card-list-item">
-          <Link className="about__card-link">
-            <div className="about__card-link-top">
-              <span className="about__card-link-top-title">MArketing</span>
-            </div>
-            <div className="about__card-link-bottom">
-              <div className="about__card-link-bottom-left">
-                <h3 className="about__card-link-bottom-left-title">
-                  SMM onlayn
-                </h3>
-                <p className="about__card-link-bottom-left-subtitle">
-                  Ijtimoiy tarmoqlar orqali turli biznes va loyihalarni
-                  rivojlantirishni o'rganasiz.
-                </p>
-              </div>
-              <div className="about__card-link-bottom-right">
-                <img
-                  src="https://cdn.najottalim.uz/najottalim/courses/illustrations/95b2a0a8-84b1-4257-8006-875a0351f561.svg"
-                  alt=""
-                />
-              </div>
-            </div>
-          </Link>
-        </li>
-      </ul>
-      <ul className="about__card-list">
-        <li className="about__card-list-item">
-          <Link className="about__card-link">
-            <div className="about__card-link-top">
-              <span className="about__card-link-top-title">MArketing</span>
-            </div>
-            <div className="about__card-link-bottom">
-              <div className="about__card-link-bottom-left">
-                <h3 className="about__card-link-bottom-left-title">
-                  SMM onlayn
-                </h3>
-                <p className="about__card-link-bottom-left-subtitle">
-                  Ijtimoiy tarmoqlar orqali turli biznes va loyihalarni
-                  rivojlantirishni o'rganasiz.
-                </p>
-              </div>
-              <div className="about__card-link-bottom-right">
-                <img
-                  src="https://cdn.najottalim.uz/najottalim/courses/illustrations/95b2a0a8-84b1-4257-8006-875a0351f561.svg"
-                  alt=""
-                />
-              </div>
-            </div>
-          </Link>
-        </li>
-      </ul>
-      <ul className="about__card-list">
-        <li className="about__card-list-item">
-          <Link className="about__card-link">
-            <div className="about__card-link-top">
-              <span className="about__card-link-top-title">MArketing</span>
-            </div>
-            <div className="about__card-link-bottom">
-              <div className="about__card-link-bottom-left">
-                <h3 className="about__card-link-bottom-left-title">
-                  SMM onlayn
-                </h3>
-                <p className="about__card-link-bottom-left-subtitle">
-                  Ijtimoiy tarmoqlar orqali turli biznes va loyihalarni
-                  rivojlantirishni o'rganasiz.
-                </p>
-              </div>
-              <div className="about__card-link-bottom-right">
-                <img
+                  className="about__card-link-bottom-right-images"
                   src="https://cdn.najottalim.uz/najottalim/courses/illustrations/95b2a0a8-84b1-4257-8006-875a0351f561.svg"
                   alt=""
                 />
