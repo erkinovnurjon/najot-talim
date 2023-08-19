79d333a07f3e199031f8955fb23e37ba8ab98b40
Nurjon <nurjon2477@gmail.com>
2023-08-19 16:35:19 +0500

    header end

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,7 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
    <>
-   
-  
-       <div className="navbar">
+            <div className="navbar">
                 <div>
                  < Link to={'/'} >
                     <img className="navbar-logo" src="https://najottalim.uz/_next/static/media/logo-img.2935c262.svg" alt="logo" />
