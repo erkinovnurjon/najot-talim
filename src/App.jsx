3d893952eca8b1eda6c64c377d7b0ac22a7a88c4
Nurjon <nurjon2477@gmail.com>
2023-08-19 11:08:28 +0500

    add app.jsx + pages, components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import "./assets/css/normalize.css";
+import { BrowserRouter as Router , Route , Routes} from "react-router-dom"
+
 
 function App() {
  return (
     <>
+    <Router>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/" element={<About />} />
+      <Route path="/" element={<Sign />} />
+     </Routes>
+    </Router>
     
     </>
   )
