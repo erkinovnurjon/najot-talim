0bf2381d9d112f8a3a96f11e95fb8fef19290166
anvarbeknorimov <anvarbeknorimov9846@gmail.com>
2023-08-19 12:04:35 +0500

    about-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import "./assets/css/normalize.css";
 import { BrowserRouter as Router , Route , Routes} from "react-router-dom"
+import About from "./pages/About";
+import Sign from "./pages/Sign";
+import Home from "./pages/Home";
 
 
 function App() {
@@ -8,8 +11,8 @@ function App() {
     <Router>
      <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/" element={<About />} />
-      <Route path="/" element={<Sign />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign" element={<Sign />} />
      </Routes>
     </Router>
     
---
+++ src/pages/About.jsx
@@ -0,0 +1,26 @@
+const About = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="courses__hover">
+          <ul>
+            <li>Bosh sahifa</li>
+            <li>Kurslar</li>
+          </ul>
+        </div>
+
+        <div className="hover_title">
+          <div className="hover_title-top">
+            <h1>Barcha o‘quv kurslari </h1>
+            <input type="search" placeholder="izlashBarcha o‘quv kurslari "/>
+          </div>
+          <div className="hover_title-bottom">
+
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default About
