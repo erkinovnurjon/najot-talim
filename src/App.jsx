390d0f671d8a21c29c92461265e84a5553d45549
Nurjon <nurjon2477@gmail.com>
2023-08-20 08:40:07 +0500

    addd login page pochti

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { BrowserRouter as Router , Route , Routes} from "react-router-dom"
+import { BrowserRouter as Router , Route , Routes} from "react-router-dom";
+
 import About from "./pages/About";
-import Sign from "./pages/Sign";
+
 import Home from "./pages/Home";
 import "./assets/css/normalize.css";
 import "./assets/css/main.css"
 import Blog from "./pages/Blog";
+import Email from "./pages/loginpages/Email";
+import Login from "./pages/loginpages/Login";
+import Register from "./pages/loginpages/Register";
+import RegEmail from "./pages/loginpages/RegEmail";
 
 
 function App() {
@@ -15,7 +20,11 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/cources" element={<About />} />
       <Route path="/blog" element={<Blog />} />
-      <Route path="/login" element={<Sign />} />
+      
+       <Route path="/login" element={<Login />} />
+        <Route path="/login/email" element={<Email />} />
+        <Route path="/login/register" element={<Register />} />
+        <Route path="/login/register/email" element={<RegEmail />} />
      </Routes>
     </Router>
     
