c9a8f04afa0b601d352e1e53ec0998ee4a09dbba
anvarbeknorimov <anvarbeknorimov9846@gmail.com>
2023-08-20 07:43:26 +0500

    about media

--- src/components/Aboutcard.jsx
+++ src/components/Aboutcard.jsx
@@ -12,10 +12,22 @@ const Aboutcard = () => {
               <span className="about__card-link-top-title">Marketing</span>
             </div>
             <div className="about__card-link-bottom">
-              <div className="about__card-link-bottom-left">
-                <h3 className="about__card-link-bottom-left-title">
-                  SMM onlayn
-                </h3>
+              <div>
+                <img
+                  className="about__top-images"
+                  src="https://cdn.najottalim.uz/najottalim/courses/illustrations/95b2a0a8-84b1-4257-8006-875a0351f561.svg"
+                  alt="icon"
+                />
+              </div>
+              <div className="about__card-link-bottom-left ">
+                <div className="about_card-title">
+                  <h3 className="about__card-link-bottom-left-title">
+                    SMM onlayn
+                  </h3>
+                <span className="about__card-link-top-title left-marketing">
+                   Marketing
+                </span>
+                </div>
                 <p className="about__card-link-bottom-left-subtitle">
                   Ijtimoiy tarmoqlar orqali turli biznes va loyihalarni
                   rivojlantirishni o'rganasiz.
@@ -34,7 +46,7 @@ const Aboutcard = () => {
         <li className="about__card-list-item">
           <Link className="about__card-link">
             <div className="about__card-link-top">
-              <span className="about__card-link-top-title dizayn" >Dizayn</span>
+              <span className="about__card-link-top-title dizayn">Dizayn</span>
             </div>
             <div className="about__card-link-bottom">
               <div className="about__card-link-bottom-left">
